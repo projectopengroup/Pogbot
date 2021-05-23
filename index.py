d2ed6cf734907b3376d333f186f25ddfa3b59c57
infiniteturbo <70925648+infiniteturbo@users.noreply.github.com>
2021-05-23 16:24:59 -0700

    Added index.py. The beginnings of our project.

--- index.py
+++ index.py
@@ -2,7 +2,7 @@
 import discord
 from discord.ext import commands
 
-# Define bot and it's commands prefix.
+# Define bot and it's commands prefix..
 bot = commands.Bot(command_prefix="!")
 
 @bot.event
