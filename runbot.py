788dea1f8b06f0f64ce28edbcc5dfa3266b5117c
infiniteturbo <70925648+infiniteturbo@users.noreply.github.com>
2021-05-23 19:29:46 -0700

    Trying something to make my life easier on AWS. Maybe now?

--- runbot.py
+++ runbot.py
@@ -1,4 +1,4 @@
 import os
-os.system('rm -rf code')
-os.system('git clone https://github.com/projectopengroup/Pogbot.git code')
-os.system('python3 code/index.py')
+os.remove('index.py')
+os.system('wget https://raw.githubusercontent.com/projectopengroup/Pogbot/main/index.py')
+os.system('python3 index.py')
