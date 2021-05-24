30632eaaf0e36dfb48fba2aa273274e118861e4f
infiniteturbo <70925648+infiniteturbo@users.noreply.github.com>
2021-05-23 19:20:12 -0700

    Trying something to make my life easier on AWS...

--- runbot.py
+++ runbot.py
@@ -1,4 +1,4 @@
 import os
 os.system('rm -rf code')
-os.system('git clone https://github.com/projectopengroup/Pogbot.git code')
+os.system('git clone https://github.com/projectopengroup/Pogbot.git')
 os.system('python3 code/index.py')
