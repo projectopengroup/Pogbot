595ea8f4bcd462a57e112fe35851d3046f693709
infiniteturbo <70925648+infiniteturbo@users.noreply.github.com>
2021-05-23 19:42:08 -0700

    Added comments to runbot.py script.

--- runbot.py
+++ runbot.py
@@ -1,3 +1,5 @@
+# For use on AWS only. If you're looking for project code, see index.py.
+# Quick and dirty update script.
 import os
 os.remove('index.py')
 os.system('wget https://raw.githubusercontent.com/projectopengroup/Pogbot/main/index.py')
