b13e3f2bb428446ec674317fe134ff29940ea2d3
infiniteturbo <70925648+infiniteturbo@users.noreply.github.com>
2021-05-26 18:07:28 -0700

    Reverting run.sh.

--- run.sh
+++ run.sh
@@ -1,49 +1,5 @@
 #!/bin/bash
 
-# Default to working directory
-LOCAL_REPO="."
-# Default to git pull with FF merge in quiet mode
-GIT_COMMAND="git pull --quiet"
-
-# User messages
-GU_ERROR_FETCH_FAIL="Unable to fetch the remote repository."
-GU_ERROR_UPDATE_FAIL="Unable to update the local repository."
-GU_ERROR_NO_GIT="This directory has not been initialized with Git."
-GU_INFO_REPOS_EQUAL="The local repository is current. No update is needed."
-GU_SUCCESS_REPORT="Update complete."
-
-
-if [ $# -eq 1 ]; then
-  LOCAL_REPO="$1"
-  cd "$LOCAL_REPO"
-fi
-
-if [ -d ".git" ]; then
-  # update remote tracking branch
-  git remote update >&-
-  if (( $? )); then
-      echo $GU_ERROR_FETCH_FAIL >&2
-      exit 1
-  else
-      LOCAL_SHA=$(git rev-parse --verify HEAD)
-      REMOTE_SHA=$(git rev-parse --verify FETCH_HEAD)
-      if [ $LOCAL_SHA = $REMOTE_SHA ]; then
-          echo $GU_INFO_REPOS_EQUAL
-          exit 0
-      else
-          $GIT_COMMAND
-          if (( $? )); then
-              echo $GU_ERROR_UPDATE_FAIL >&2
-              exit 1
-          else
-              echo $GU_SUCCESS_REPORT
-              pkill -9 -e -f index.py
-              python3 index.py
-          fi
-      fi
-  fi
-else
-  echo $GU_ERROR_NO_GIT >&2
-  exit 1
-fi
-exit 0
+rm -rf ./index.py
+wget https://raw.githubusercontent.com/projectopengroup/Pogbot/main/index.py
+python3 index.py
