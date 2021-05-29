0a07bb42bd82c603e6e8e5ab5defb0c6bff9e33c
TheOneCheetah <71140108+TheOneCheetah@users.noreply.github.com>
2021-05-29 12:10:12 -0500

    Updated embed function so it can be used in more situations

--- index.py
+++ index.py
@@ -129,25 +129,31 @@ async def get_prefix(client, message):
 
 # Function to create one line embeds. So far it takes most of the possible arguments that you can set to an embed. Not
 # all args need to be filled out, but a title or description is required. One of them has to be filled
-# out to create an embed, or both. To create an embed, just type ' await send_embed(ctx, arg=value) ' with the arg being
-# one of the args that it takes and the appropriate value. Any image, thumbnail, pfp, etc. takes a url in the form of a
-# string. The fields argument takes an array of tuples. So for example:
+# out to create an embed, or both. To create an embed, just type ' await send_embed(ctx, send_option=option, arg=value)'
+# with the arg being one of the args that it takes and the appropriate value. Any image, thumbnail, pfp, etc. takes a
+# url in the form of a string. The fields argument takes an array of tuples. So for example:
 #                                                           [(name1, value1, inline1), (name2, value2, inline2)]
-async def send_embed(ctx, title=None, description=None, author=None, author_pfp=None,
-                     color=discord.Colour.default(), footer=None, thumbnail=None, image=None, fields=None):
+
+# send_option = 0 means create then send the embed
+# send_option = 1 means create and send the embed, but also return the embed so that it can be stored in a variable and
+# the sent embed can be edited later
+# send_option = 2 create the embed, but only return the embed, not send it, so the embed object is stored in a variable
+# that can be used later.
+async def send_embed(ctx, send_option=0, title=None, description=None, author=None, author_pfp=None,
+                     color=discord.Colour.default(), footer=None, thumbnail=None, image=None, url=None, fields=None):
     if title is None and description is None:
         print("[Error]: Error creating embed. No title or description specified.")
     else:
         # Initializes new_embed variable
-        new_embed = None
+        new_embed = discord.Embed(colour=color)
 
-        # Checks if title and description are 'None'. If None, that element isn't added to the embed
-        if title is not None and description is not None:
-            new_embed = discord.Embed(title=title, description=description, color=color)
-        elif title is not None and description is None:
-            new_embed = discord.Embed(title=title, color=color)
-        elif title is None and description is not None:
-            new_embed = discord.Embed(description=description, color=color)
+        # Checks if title, description, and url are 'None'. If None, that element isn't added to the embed
+        if title is not None:
+            new_embed.title = title
+        if description is not None:
+            new_embed.description = description
+        if url is not None:
+            new_embed.url = url
 
         # Checks if each argument is 'None'. If None, the argument is ignored. But if not None, then the element is
         # added to the embed.
@@ -166,7 +172,12 @@ async def send_embed(ctx, title=None, description=None, author=None, author_pfp=
             for field in fields:
                 new_embed.add_field(name=field[0], value=field[1], inline=field[2])
 
-        await ctx.send(embed=new_embed)
+        if send_option == 0:
+            await ctx.send(embed=new_embed)
+        elif send_option == 1:
+            return await ctx.send(embed=new_embed)
+        elif send_option == 2:
+            return new_embed
 
 
 # Define bot and it's commands prefix, calling the get_prefix function, where it returns the server specific prefix.
@@ -283,15 +294,16 @@ async def setup(ctx):
         # Sleep the thread for a little bit so we can edit the message.
         await asyncio.sleep(0.4)
         # Form the embed that we want to use for our edit.
-        embededit = discord.Embed(title=f"**Pogbot Setup**", description="Respond with any menu option to proceed.",
-                                  color=0x08d5f7)
-        embededit.set_thumbnail(url='https://i.imgur.com/rYKYpDw.png')
-        embededit.add_field(name='Settings', value="Basic server settings.")
-        embededit.add_field(name='Moderator', value="Moderator settings.")
-        embededit.add_field(name='Reactions', value="Setup role reactions.")
-        embededit.add_field(name='Commands', value="Configure custom commands.")
-        embededit.add_field(name='Logs', value="Enable event logs.")
-        embededit.add_field(name='Switcher', value="Turn on/off commands.")
+        embededit = await send_embed(ctx, send_option=2, title=f"**Pogbot Setup**", color=0x08d5f7,
+                                     thumbnail='https://i.imgur.com/rYKYpDw.png',
+                                     description="Respond with any menu option to proceed.",
+                                     fields=[('Settings', 'Basic server settings.', True),
+                                             ('Moderator', "Moderator settings.", True),
+                                             ('Reactions', "Setup role reactions.", True),
+                                             ('Commands', "Configure custom commands.", True),
+                                             ('Logs', "Enable event logs.", True),
+                                             ('Switcher', "Turn on/off commands.", True)
+                                             ])
         # Setting our global pogsetup var to true.
         pogsetup = True
         orginchannel = ctx.message.channel
@@ -358,13 +370,12 @@ async def on_message(msg):
     if "Direct Message" in str(msg.channel):
         # If any of the IDs match Mag, Cheetah, or Jonny then
         if "join" in msg.content:
-            embededadd = discord.Embed(title=f"**Click here to add Pogbot to your server**",
-                                       url="https://discord.com/api/oauth2/authorize?client_id=843272975771631616"
-                                           "&permissions=0&scope=bot",
-                                       description="The default prefix is ! \n Run the command !setup once added to "
-                                                   "get started.", color=0x08d5f7)
             # https://discord.com/api/oauth2/authorize?client_id=843272975771631616&permissions=0&scope=bot
-            await msg.channel.send(embed=embededadd)
+            await send_embed(msg.channel, send_option=0, title=f"**Click here to add Pogbot to your server**",
+                             url="https://discord.com/api/oauth2/authorize?client_id=843272975771631616"
+                                 "&permissions=0&scope=bot",
+                             description="The default prefix is ! \n Run the command !setup once added to "
+                                         "get started.", color=0x08d5f7)
         if str(msg.author.id) == "421781675727388672" or "171238557417996289" or "293362579709886465":
             # Look for the text "reboot" in the message
             if "reboot" in msg.content:
@@ -414,11 +425,11 @@ async def on_message(msg):
                 # Look for the word set in the message that has been converted into lower case.
                 if "set" in str(msg.content.lower()):
                     # If it's found then form our embed.
-                    embededit = discord.Embed(title=f"**Basic Settings**",
-                                              description="Respond with any menu option to proceed.", color=0x08d5f7)
-                    embededit.set_thumbnail(url='https://i.imgur.com/rYKYpDw.png')
-                    embededit.add_field(name='Prefix', value="Set the bots prefix.")
-                    embededit.add_field(name='Welcomes', value="Setup a channel for welcome messages.")
+                    embededit = await send_embed(msg.channel, send_option=2, title=f"**Basic Settings**",
+                                                 thumbnail='https://i.imgur.com/rYKYpDw.png', color=0x08d5f7,
+                                                 description="Respond with any menu option to proceed.",
+                                                 fields=[('Prefix', "Set the bots prefix.", True),
+                                                         ('Welcomes', "Setup a channel for welcome messages.", True)])
                     # Edit the original message.
                     await pogsetupid.edit(embed=embededit)
                     # Delete the message.
@@ -426,12 +437,12 @@ async def on_message(msg):
                 # Look for pre in lowercase message
                 if "pre" in str(msg.content.lower()):
                     # If it's found then form the embed.
-                    embededit = discord.Embed(title=f"**Prefix Setting**",
-                                              description="Respond with a new prefix for the bot.", color=0x08d5f7)
-                    embededit.set_thumbnail(url='https://i.imgur.com/rYKYpDw.png')
-
-                    embededit.add_field(name='Current Prefix', value=f"{prefix[0]}")
+                    embededit = await send_embed(msg.channel, send_option=2, title=f"**Prefix Setting**",
+                                                 description="Respond with a new prefix for the bot.", color=0x08d5f7,
+                                                 thumbnail='https://i.imgur.com/rYKYpDw.png',
+                                                 fields=[('Current Prefix', f"{prefix[0]}", True)])
                     # Edit the message.
+
                     await pogsetupid.edit(embed=embededit)
                     # Delete the user message.
                     await msg.delete()
