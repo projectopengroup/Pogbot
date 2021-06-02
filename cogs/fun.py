2258d027a28b1d92742b68f481b36ae5671dbe65
infiniteturbo <70925648+infiniteturbo@users.noreply.github.com>
2021-06-01 18:42:43 -0700

    Spock Jokes.

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,5 +1,6 @@
 import aiohttp
 import requests
+import random
 from discord.ext import commands
 from utils.pogfunctions import send_embed
 
@@ -42,8 +43,8 @@ class Fun(commands.Cog, name="fun"):
         else:
             await send_embed(ctx, title=str(joke["setup"]), description=str(joke["delivery"]), color=0x08d5f7)
 
-    @commands.command(name='idea', aliases=['bored', 'activity'], brief='Suggests and activity.',
-                      description="Suggests and activity for you to do when you're bored.")
+    @commands.command(name='idea', aliases=['bored', 'activity'], brief='Suggests an activity.',
+                      description="Suggests an activity for you to do when you're bored.")
     # Look for a command called activity
     async def idea(self, ctx):
         request = requests.get(url=f'https://www.boredapi.com/api/activity')
@@ -57,6 +58,22 @@ class Fun(commands.Cog, name="fun"):
             print(activity['link'])
             await send_embed(ctx, title=str(activity['activity']), url=link,
                              color=0x08d5f7)
+
+    @commands.command(name='spock', aliases=['sjoke', 'spockjoke', 'sjokes'], brief='Responds with a Spock Joke.',
+                      description="Responds with a joke that pertains to Spock.")
+    async def spock(self, ctx):
+        spock = ["Spock had three ears: a left ear, a right ear, and a final front ear",
+                 "What did Spock find in the toilet of the Enterprise? \n **The captain's log.**",
+                 "What does Spock use as birth control? \n **Vulcanized Rubber.**",
+                 "Why doesn't Spock give hand jobs? \n **Because his Vulcan grip will make you limp**",
+                 "What do Captain Kirk and Mister Spock do to get their baggage up to their hotel room? \n **Tell a "
+                 "porter.**",
+                 "Why didn't Spock do a mind meld with Frodo? \n **Because he figured that would be a bad hobbit to "
+                 "get in to.**",
+                 "**Yo Mama's so fat, even Spock thought she outweighed the needs of the many!**",
+                 "If the shocker don't rock her. \n Spock Her."]
+        rindex = random.randint(0, len(spock) - 1)
+        await send_embed(ctx, title=spock[rindex], color=0x08d5f7)
 
 
 def setup(bot):
