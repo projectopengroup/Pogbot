14c3a240e37ac849a1280378a905e4528e18d051
infiniteturbo <70925648+infiniteturbo@users.noreply.github.com>
2021-06-03 16:56:24 -0700

    Welcome card customization version 1.2

--- cogs/config.py
+++ cogs/config.py
@@ -35,7 +35,9 @@ class Config(commands.Cog, name="config"):
         await ctx.send(file=create_welcome_card(avatarRequest, user, ctx.guild))
 
     @commands.command(name='cbg', aliases=['cardbg', 'cardbackground'], brief="Change welcome card background",
-                      description="Allows users to set their welcome card backgrounds to image or color. \n "
+                      description="Allows users to set their welcome card backgrounds to image url or html color. \n "
+                                  "**Color** must be HTML color code: e.g. #b3995d"
+                                  '**Image** must be a valid url to an image file that ends in .jpg, .png, etc.\n \n'
                                   "Suggested size: \n Width: 1000 "
                                   " Height: 370")
     async def cbg(self, ctx, *, imageurlorcolor):
@@ -65,7 +67,8 @@ class Config(commands.Cog, name="config"):
             await ctx.send(embed=embederror)
 
     @commands.command(name='cbc', aliases=['cbcolor', 'bannercolor', 'cardbcolor', 'cardbannercolor'], brief="Change welcome card banner color",
-                      description="Allows users to set their welcome card banner colors.")
+                      description="Allows users to set their welcome card banner colors.\n"
+                                  "Color must be a HTML color code: e.g. #b3995d")
     async def cbc(self, ctx, *, htmlcolorcode):
         checkcolor = re.search(r'^#(?:[0-9a-fA-F]{3}){1,2}$', htmlcolorcode)
         if checkcolor:
