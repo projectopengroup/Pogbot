c892a11b67bca5e0cad1f2ecd3347f2c1c00eab8
infiniteturbo <70925648+infiniteturbo@users.noreply.github.com>
2021-06-05 18:12:03 -0700

    Updates to command error handler.

--- cogs/commanderrorhandler.py
+++ cogs/commanderrorhandler.py
@@ -7,7 +7,6 @@ from utils.pogfunctions import send_embed
 
 
 class CommandErrorHandler(commands.Cog):
-
     def __init__(self, bot):
         self.bot = bot
 
@@ -30,7 +29,7 @@ class CommandErrorHandler(commands.Cog):
         if isinstance(error, commands.DisabledCommand):
             errorsend = await send_embed(ctx, send_option=2,
                                          description=f"<:Pogbot_X:850089728018874368> "
-                                                     f"{ctx.command} has been disabled.",
+                                                     f"**{ctx.command} has been disabled.**",
                                          color=0x08d5f7)
             await ctx.send(embed=errorsend)
 
@@ -38,7 +37,7 @@ class CommandErrorHandler(commands.Cog):
             try:
                 errorsend = await send_embed(ctx, send_option=2,
                                              description=f"<:Pogbot_X:850089728018874368> "
-                                                         f"{ctx.command} can not be used in Direct Messages.",
+                                                         f"**{ctx.command} can not be used in Direct Messages.**",
                                              color=0x08d5f7)
                 await ctx.author.send(embed=errorsend)
             except discord.HTTPException:
@@ -48,13 +47,20 @@ class CommandErrorHandler(commands.Cog):
             if ctx.command.qualified_name == 'tag list':
                 errorsend = await send_embed(ctx, send_option=2,
                                              description=f"<:Pogbot_X:850089728018874368> "
-                                                         f"that member cannot be found. Try again.",
+                                                         f"**that member cannot be found. Try again.**",
                                              color=0x08d5f7)
                 await ctx.send(embed=errorsend)
 
         else:
             print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
             traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+
+        if isinstance(error, commands.MissingRequiredArgument):
+            errorsend = await send_embed(ctx, send_option=2,
+                                         description=f"<:Pogbot_X:850089728018874368> "
+                                                     f"**you must provide an argument.**",
+                                         color=0x08d5f7)
+            await ctx.send(embed=errorsend)
 
 
 def setup(bot):
