aa486e9573a24c6f36488a20ea46bad4e9e89b12
infiniteturbo <70925648+infiniteturbo@users.noreply.github.com>
2021-06-24 10:52:21 -0700

    Final music push.

--- cogs/music.py
+++ cogs/music.py
@@ -45,6 +45,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
     }
 
     FFMPEG_OPTIONS = {
+        'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
         'options': '-vn',
     }
 
