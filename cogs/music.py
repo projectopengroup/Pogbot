be4913130c3ef7efa9c5aec017ba8e9204f0ec54
infiniteturbo <70925648+infiniteturbo@users.noreply.github.com>
2021-06-24 12:13:10 -0700

    Fixed some stuff, and added proper credits.

--- cogs/music.py
+++ cogs/music.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (c) 2019 Valentin B.
-A simple music bot written in discord.py using youtube-dl.
-Though it's a simple example, music bots are complex and require much time and knowledge until they work perfectly.
-Use this as an example or a base for your own bot and extend it as you want. If there are any bugs, please let me know.
-Requirements:
-Python 3.5+
-pip install -U discord.py pynacl youtube-dl
-You also need FFmpeg in your PATH environment variable or the FFmpeg.exe binary in your bot's directory on Windows.
+This code originates from music_bot_example.py a discord music bot example Copyright (c) 2019 Valentin B.
 """
 
 import asyncio
